6b3fad40021dcfb1ce51d9f4eca72e6d291b3960
Asar Zia <asarzia45@gmail.com>
2023-03-30 13:03:23 -0700

    Application completed

--- lib/View/Splash_Screen.dart
+++ lib/View/Splash_Screen.dart
@@ -12,7 +12,7 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMixin {
+class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMixin{
   late final AnimationController _controller =AnimationController(
     duration: Duration(seconds: 3),
       vsync: this)..repeat();
